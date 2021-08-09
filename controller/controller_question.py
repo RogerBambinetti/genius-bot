44211ef92c96cd9133dece1307fcb7299549b176
Rafa <rafaela.tillmann@ozmap.com.br>
2021-08-08 22:33:08 -0300

    entidade question

---
+++ controller/controller_question.py
@@ -0,0 +1,19 @@
+from model.question import Question
+
+
+class ControllerQuestion:
+
+    def __init__(self):
+        pass
+
+    def listQuestions(self):
+        pass
+
+    def createQuestion(self):
+        pass
+
+    def updateQuestion(self):
+        pass
+
+    def deleteQuestion(self):
+        pass
---
+++ model/question.py
@@ -0,0 +1,53 @@
+from model.category import Category
+from datetime import date
+
+
+class Question:
+
+    def __init__(self, description: str, answer: str, category: Category, points: int, date: date):
+        self.__description = description
+        self.__answer = answer
+        if isinstance(category, Category):
+            self.__category = category
+        self.__points = points
+        self.__date = date
+
+    @property
+    def description(self):
+        return self.__description
+
+    @description.setter
+    def description(self, description: str):
+        self.__description = description
+
+    @property
+    def answer(self):
+        return self.__answer
+
+    @answer.setter
+    def answer(self, answer: str):
+        self.__answer = answer
+
+    @property
+    def category(self):
+        return self.__category
+
+    @category.setter
+    def category(self, category: Category):
+        self.__category = category
+
+    @property
+    def points(self):
+        return self.__points
+
+    @points.setter
+    def points(self, points: int):
+        self.__points = points
+
+    @property
+    def date(self):
+        return self.__date
+
+    @date.setter
+    def date(self, date: date):
+        self.__date = date
