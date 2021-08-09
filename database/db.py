9ab072cb1ffa73fef33be6d340a18e2417a56551
roger <roger.bambinetti@devoz.com.br>
2021-08-08 22:27:25 -0300

    alteracoes na db

--- database/db.py
+++ database/db.py
@@ -1,11 +1,12 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
+from sqlalchemy.ext.declarative import declarative_base
 
 class Database():
     def __init__(self):
         self.__engine = create_engine('sqlite:///database.db', echo=True)
-        Session = sessionmaker(bind=self.__engine)
-        self.__session = Session()
+        self.__session = sessionmaker(bind=self.__engine)
+        self.__base = declarative_base(bind=self.__engine)
 
     @property
     def engine(self):
@@ -14,3 +15,7 @@ class Database():
     @property
     def session(self):
         return self.__session
+
+    @property
+    def base(self):
+        return self.__base
