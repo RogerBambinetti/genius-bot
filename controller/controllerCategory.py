a437e04fb1c457966d23a4efcac9b24812067793
Rafa <rafaelatillmann.rt@gmail.com>
2021-08-12 16:40:56 -0300

    continuando o controller category

--- controller/controllerCategory.py
+++ controller/controllerCategory.py
@@ -1,7 +1,24 @@
 from model.category import Category
+from model.daoCategory import Dao
 
 
 class ControllerCategory:
 
     def __init__(self):
-        pass
+        self.__dao = Dao
+
+    def create(self, name: str):
+        category = Category(name)
+        self.__dao.insert(category)
+
+    def update(self, id: int, name: str):
+        category = self.__dao.read(id)
+        category.name = name
+        self.__dao.update(category)
+
+    def delete(self, id: int):
+        category = self.__dao.read(id)
+        self.__dao.delete(category)
+
+    def listCategories(self):
+        return self.__dao.list()
