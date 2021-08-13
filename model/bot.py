c96f824e1ff45d49160a5739d7ccac3e25269bcc
roger <roger.bambinetti@devoz.com.br>
2021-08-12 21:21:15 -0300

    criando bot

---
+++ model/bot.py
@@ -0,0 +1,11 @@
+import tweepy
+
+class Bot():
+    def __init__(self):
+        self.__auth = tweepy.OAuthHandler("eRUT7A5TeY1Dj4u3cTtMnMIFx", "rtVacS2yMotxcy1V2Z7YDMQbHtEXbBADWGebF2iDQPYfDLJ3KT")
+        self.__auth.set_access_token("1425438439722430469-DLQ5T6LSCtf8hSnRYRSrDBGjCjsxyA", "rNFZjZonQfcqkfOsli0B6KNWLiR6YRB4EVV4brrVi30Jv")
+        self.__api = tweepy.API(self.__auth)
+    
+    @property
+    def api(self):
+        return self.__api
